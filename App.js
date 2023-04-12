88701f522a4974a3fced4e395596539073c2a265
Nishant <nishantchaudhary5338@gmail.com>
2023-04-12 12:57:45 +0530

    its working

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { StyleSheet, Dimensions, View, Button } from 'react-native';
+import { StyleSheet, Dimensions, View, Button, SafeAreaView } from 'react-native';
 import * as openAnything from 'react-native-openanything';
 
 
 
 function App() {
     return(
-        <View>
+        <SafeAreaView>
+            <View>
             <Button title = "Open Pdf"
               onPress = {()=> openAnything.Pdf("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf")}
             >
 
             </Button>
         </View>
+        </SafeAreaView>
     )
 }
 
